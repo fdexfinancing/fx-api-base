05e7c4f2906d2a079912436db9e5f3124bc05abc
Murillo <mmbfreitas@hotmail.com>
2016-10-05 11:26:23 -0300

    receive secret in constructor

--- src/auth.js
+++ src/auth.js
@@ -3,9 +3,11 @@
 const crypto = require('crypto');
 const bcrypt = require('bcrypt-nodejs');
 const jwt = require('jsonwebtoken');
-const jwtSecret = require("./config.json").jwtSecret;
 
 class Auth {
+    constructor(jwtSecret) {
+        this.jwtSecret = jwtSecret
+    }
 
     verifyPassword(password, hash, callback) {
 
@@ -49,7 +51,7 @@ class Auth {
             id: object.id,
             name: object.name,
             email: object.email
-        }, jwtSecret, {
+        }, this.jwtSecret, {
             expiresIn: 60*60*24*30, // 30 dias
             audience
         });
@@ -60,7 +62,7 @@ class Auth {
         try {
 
             // verify a token
-            jwt.verify(oldToken, jwtSecret, (err, profile) => {
+            jwt.verify(oldToken, this.jwtSecret, (err, profile) => {
 
                 if (err && err.name == 'TokenExpiredError') {
                     profile = jwt.decode(oldToken);
@@ -74,7 +76,7 @@ class Auth {
                     id: profile.id,
                     email: profile.email,
                     name: profile.name
-                }, jwtSecret, {
+                }, this.jwtSecret, {
                     expiresIn: 60 * 60 * 24 * 30,
                     audience: profile.aud
                 });
